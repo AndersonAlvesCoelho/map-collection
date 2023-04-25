3ce9bb75b9e2d361395d002dfde90b278bdfc1f7
andersonalvescoelho <a0a0coelho0@gmail.com>
2023-04-25 20:55:36 -0300

    merge

--- src/Layouts/LayoutNavigation.tsx
+++ src/Layouts/LayoutNavigation.tsx
@@ -13,9 +13,7 @@ export default function LayoutNavigation() {
         <main className="relative md:ml-64">
           <Header />
 
-          <div className="px-4 md:px-10 mx-auto w-full -m-24 md:pt-32 pb-32 pt-12">
-            <Outlet />
-          </div>
+          <Outlet />
         </main>
       </div>
     </DarkModeContextProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 export default function HomePage() {
   return (
-    <>
-      {Array(150)
-        .fill(null)
-        .map((_, index) => (
-          <h1 >Hello Word {index}</h1>
-        ))}
-    </>
+    // <iframe
+    //   src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15001.943662766422!2d-43.9150582!3d-19.946057099999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1682465836079!5m2!1spt-BR!2sbr"
+    //   className="h-full w-full"
+    // ></iframe>
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15001.943662766422!2d-43.9150582!3d-19.946057099999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1682465836079!5m2!1spt-BR!2sbr"
+      width="100%"
+      height="600"
+    ></iframe>
   );
 }
