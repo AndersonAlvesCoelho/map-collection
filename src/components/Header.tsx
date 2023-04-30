ed983e444001b36fc764173f935d0fcd93c30d3b
andersonalvescoelho <a0a0coelho0@gmail.com>
2023-04-29 23:07:23 -0300

    (Refector)[tailwind.config]: Alterado paleta do cores

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,26 @@
-// IMPORTS
-import { useState, useEffect } from "react";
-
 //ASSETS
-import {Star, Clock, Paperclip, MapPin } from "lucide-react";
+import { Star, Clock, Paperclip, MapPin } from "lucide-react";
 
 export default function Header() {
- 
-
   return (
-    <header className="items-center p-4 border-b-2 shadow-sm border-concrete-900 bg-[#393646] dark:bg-gray-800 ">
+    <header className="items-center p-4 border-b-2 shadow-sm border bg-viridian-50 ">
       <div className="w-full mx-auto items-center flex justify-between px-4">
-        <a className="block text-teal-600" href="/">
+        <a className="p-2 outline-0 rounded-md hover:outline-1 hover:bg-viridian-200 active:bg-viridian-500">
           <span className="sr-only">Home</span>
           <MapPin color="black" size={32} />
         </a>
 
         <div className="flex items-center justify-between">
           <nav className="flex items-center gap-6 text-sm">
-            <a className="p-2 outline-0 outline-zinc-500 rounded-md hover:outline-1 hover:bg-slate-300/20">
-              <Star color="black" size={16} />
+            <a className="p-2 rounded-md hover:scale-105 hover:bg-viridian-200 active:bg-viridian-500 transition duration-800 ease-in-out">
+              <Star size={16} className="text-dune-900" />
             </a>
-            <a className="p-2 outline-0 outline-zinc-500 rounded-md hover:outline-1 hover:bg-slate-300/20">
-              <Clock color="black" size={16} />
+            <a className="p-2 outline-0 rounded-md hover:outline-1 hover:bg-viridian-200 active:bg-viridian-500">
+              <Clock size={16} className="text-dune-900" />
             </a>
             <div className="bg-gray-500 w-0.5 py-2.5"></div>
-            <a className="p-2 outline-0 outline-zinc-500 rounded-md hover:outline-1 hover:bg-slate-300/20">
-              <Paperclip
-                color="black"
-                size={16}
-              />
+            <a className="p-2 outline-0 rounded-md hover:outline-1 hover:bg-viridian-200 active:bg-viridian-500">
+              <Paperclip size={16} className="text-dune-900" />
             </a>
           </nav>
         </div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,54 +1,44 @@
 // CONTEXT
-import { useDarkMode } from "../context/DarkModeContext";
+import { SearchMapModal } from ".";
 
 // ASSETS
 import { Map, LocateFixed, ChevronDown } from "lucide-react";
 
 export default function Sidebar() {
-
   return (
-    <aside className="left-0 top-0 bottom-0 block fixed flex-row flex-nowrap overflow-y-auto overflow-hidden w-64 border-b-2 border-gray-400 shadow-sm bg-gray-100 dark:bg-gray-800">
+    <aside className="left-0 top-0 bottom-0 block fixed flex-row flex-nowrap overflow-y-auto overflow-hidden w-64 shadow-sm border bg-viridian-50">
       <nav className="mt-6 flex flex-col gap-2 px-2">
-        <h1 className="flex items-center gap-2 rounded-lg px-4 py-2 bg-zinc-900/5 hover:bg-zinc-900/5">
-          <LocateFixed color="black" size={16} />
-          <span className="text-xl text-gray-500 hover:text-gray-700">
-            {" "}
-            Banned Users{" "}
-          </span>
+        <h1 className="flex items-center gap-2 px-4 py-2 rounded-lg bg-viridian-500 ">
+          <LocateFixed size={18} className="text-chamois-50" />
+          <span className="text-xl text-chamois-50">Banned Users</span>
         </h1>
 
-        <SearchMapModal />
+        {/* <SearchMapModal /> */}
 
         <hr className="my-4 md:min-w-full" />
 
         <details className="group [&_summary::-webkit-details-marker]:hidden">
-          <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-zinc-200 hover:text-gray-700">
+          <summary className="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2">
             <div className="flex items-center gap-2">
-              <LocateFixed
-                color="black"
-                size={16}
-              />
-              <span className="text-sm font-medium"> Teams </span>
+              <LocateFixed color="black" size={16} />
+              <span className="text-sm font-medium text-dune-900"> Teams </span>
             </div>
 
             <span className="shrink-0 transition duration-300 group-open:-rotate-180">
-              <ChevronDown
-                color="black"
-                size={16}
-              />
+              <ChevronDown color="black" size={16} />
             </span>
           </summary>
 
           <nav aria-label="Teams Nav" className="mt-2 flex flex-col px-4">
             <a
               href="#"
-              className="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-zinc-200 hover:text-gray-700"
+              className="flex items-center gap-2 px-4 py-2 rounded-md hover:scale-105 hover:bg-viridian-200 active:bg-viridian-500 transition duration-800 ease-in-out"
             >
-              <LocateFixed
-                color="black"
-                size={16}
-              />
-              <span className="text-sm font-medium"> Banned Users </span>
+              <LocateFixed color="black" size={16} />
+              <span className="text-sm font-medium text-dune-900">
+                {" "}
+                Banned Users{" "}
+              </span>
             </a>
           </nav>
         </details>
@@ -59,10 +49,13 @@ export default function Sidebar() {
             <a
               key={index}
               href="#"
-              className="flex items-center gap-2 rounded-lg px-4 py-2 text-gray-500 hover:bg-zinc-200 hover:text-gray-700"
+              className="flex items-center gap-2 px-4 py-2 rounded-md hover:scale-105 hover:bg-viridian-200 active:bg-viridian-500 transition duration-800 ease-in-out"
             >
               <Map color="black" size={16} />
-              <span className="text-sm font-medium"> Billing {index}</span>
+              <span className="text-sm font-medium text-dune-900">
+                {" "}
+                Billing {index}
+              </span>
             </a>
           ))}
       </nav>
